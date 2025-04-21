9b68441178c5d1bd04956e69bce5931e73b88806
devaddic <a.azeez.silmy@gmail.com>
2025-04-21 00:29:47 +0000

    environment key reference updated

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const app = express()
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  //res.send()
 });
 
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`listening on port ${port}`)
 });
 
 
